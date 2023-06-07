f6e6d4fd7a4551eec0e2fa4c659580968742d074
okik4zuya <okik4zuya@gmail.com>
2023-06-07 12:00:25 +0700

    add user id column in Users/Index

--- resources/js/Pages/Account/Users/Index.jsx
+++ resources/js/Pages/Account/Users/Index.jsx
@@ -61,6 +61,7 @@ export default function UserIndex() {
                                             <th scope="col" style={{ width: '5%' }}>No.</th>
                                             <th scope="col" style={{ width: '15%' }}>Name</th>
                                             <th scope="col" style={{ width: '15%' }}>Username</th>
+                                            <th scope="col" style={{ width: '15%' }}>User Id</th>
                                             <th scope="col" style={{ width: '30%' }}>Role</th>
                                             <th scope="col" style={{ width: '15%' }}>Actions</th>
                                         </tr>
@@ -71,6 +72,7 @@ export default function UserIndex() {
                                                     <td className="text-center">{++index + (users.current_page-1) * users.per_page}</td>
                                                     <td>{user.name}</td>
                                                     <td>{user.username}</td>
+                                                    <td>{user.id}</td>
                                                     <td>
                                                         {user.roles.map((role, index) => (
                                                             <span className="btn btn-success btn-sm shadow-sm border-0 ms-2 mb-2" key={index}>
