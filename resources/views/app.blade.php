eb89b88bc6df4448521ab5f39d26ad26f7c2a2bf
okik4zuya <okik4zuya@gmail.com>
2023-06-08 13:41:58 +0700

    add fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/assets/css/styles.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('/assets/css/fonts.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/index.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     {!! SEO::generate() !!}
     {{-- <link rel="shortcuticon" href="{{ asset('/assets/images/logo.png')}}" /> --}}
